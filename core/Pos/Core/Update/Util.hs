bb00888330479328ef6f7674aa3dba2bdb86ac5b
Alexander Vieth <aovieth@gmail.com>
2018-02-15 10:34:00 -0500

    delete duplicate check in update

--- core/Pos/Core/Update/Util.hs
+++ core/Pos/Core/Update/Util.hs
@@ -105,7 +105,6 @@ checkUpdateProposal it = do
     unless (checkSig SignUSProposal (upFrom it) toSign (upSignature it))
         (throwError "UpdateProposal: invalid signature")
     checkSoftwareVersion (upSoftwareVersion it)
-    forM_ (HM.keys (upData it)) checkSystemTag
 
 mkUpdateProposalWSign
     :: (HasConfiguration, Bi UpdateProposalToSign)
